b941647eeae6400d755b8763d43f83312bc6cd73
Erik930 <ebockwin@smail.uni-koeln.de>
2025-12-19 12:55:15 +0100

    Hausaufgabe09

--- src/h1/PrioListe.java
+++ src/h1/PrioListe.java
@@ -1,5 +1,38 @@
 package h1;
 
+import java.util.ArrayList;
+
 public class PrioListe {
 
+    public ArrayList<Patient> myList;
+
+    public PrioListe() {
+        myList = new ArrayList<Patient>();
+    }
+
+    public void addPatient(Patient p) {
+        if (myList.isEmpty()) {
+            myList.add(p);
+            return;
+        }
+
+        for (int i = 0; i < myList.size(); i++) {
+            if (p.prio < myList.get(i).prio) {
+                myList.add(i, p);
+                return;
+            }
+        }
+        myList.add(p);
+    }
+
+    public Patient getNextPatient() {
+        if (myList.isEmpty()) {
+            return null;
+        }
+        return myList.remove(0);
+    }
+
+    public int getPosition(Patient p) {
+        return myList.indexOf(p);
+    }
 }
--- src/h2/Bus.java
+++ src/h2/Bus.java
@@ -1,5 +1,69 @@
 package h2;
 
+import java.util.ArrayList;
+
 public class Bus {
 
+    public ArrayList<Passenger> passengers;
+
+    public Bus() {
+        passengers = new ArrayList<Passenger>();
+    }
+
+    public void enterBus(Passenger p) {
+        passengers.add(p);
+    }
+
+    private void exitBus() {
+        for (int i = 0; i < passengers.size(); i++) {
+            if (passengers.get(i).visited == passengers.get(i).planned) {
+                passengers.remove(i);
+                i--;
+            }
+        }
+    }
+
+    public void nextStop(Passenger[] boarding) {
+        for (Passenger p : passengers) {
+            p.visited++;
+        }
+
+        exitBus();
+
+        if (boarding != null) {
+            for (Passenger p : boarding) {
+                passengers.add(p);
+            }
+        }
+    }
+
+    public void nextStop() {
+        nextStop(null);
+    }
+
+    public ArrayList<Passenger> findPassengersWithoutTickets() {
+        ArrayList<Passenger> removed = new ArrayList<Passenger>();
+
+        for (int i = 0; i < passengers.size(); i++) {
+            if (!passengers.get(i).ticket) {
+                removed.add(passengers.get(i));
+                passengers.remove(i);
+                i--;
+            }
+        }
+        return removed;
+    }
+
+    public void transferPassengers(Bus otherBus, String[] passengerNames) {
+        for (int i = 0; i < passengers.size(); i++) {
+            for (String name : passengerNames) {
+                if (passengers.get(i).name.equals(name)) {
+                    otherBus.enterBus(passengers.get(i));
+                    passengers.remove(i);
+                    i--;
+                    break;
+                }
+            }
+        }
+    }
 }
